fe61b2bee114b4ccdead3338485f3baade58a06f
flover.nix <alexzsk@outlook.com>
2024-08-30 02:37:20 -0400

    Update home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,96 +1,3 @@
-{
-  pkgs,
-  config,
-  host,
-  username,
-  lib,
-  ...
-}: let
-  inherit (lib) mkOption types;
-in {
-  options.hmModules.core.bash = mkOption {
-    enabled = mkOption {
-      type = types.bool;
-      default = true;
-    };
-  };
-  config = {
-    programs.bash = {
-      enable = true;
-      enableCompletion = true;
-      profileExtra = ''
-
-      '';
-      shellAliases = {
-     ff = "fastfetch";     
-      };
-    };
-  };
-}
-
-[alex@glove:~]$ vim .config/home-manager/bash/default.
-nix
-
-[alex@glove:~]$ home-manager switch
-these 4 derivations will be built:
-  /nix/store/5bbbmakmav9qzp4wqs48nxn1jvywp05l-profile.drv
-  /nix/store/m8hd5hbgqa2agxyf0ckkwmmg93r5kfpm-bashrc.drv
-  /nix/store/ybmbbg2zydzrqvz9fhmxgpsn7cjmkgc6-home-manager-files.drv
-  /nix/store/77p716wi5aavvzvfg8wv1bpw91jmakyg-home-manager-generation.drv
-building '/nix/store/m8hd5hbgqa2agxyf0ckkwmmg93r5kfpm-bashrc.drv'...
-building '/nix/store/5bbbmakmav9qzp4wqs48nxn1jvywp05l-profile.drv'...
-building '/nix/store/ybmbbg2zydzrqvz9fhmxgpsn7cjmkgc6-home-manager-files.drv'...
-building '/nix/store/77p716wi5aavvzvfg8wv1bpw91jmakyg-home-manager-generation.drv'...
-/nix/store/bvmk7cfv7vj2410wrpsz24kbdfxq9b03-home-manager-generation
-Starting Home Manager activation
-Activating checkFilesChanged
-Activating checkLinkTargets
-Activating writeBoundary
-Activating installPackages
-replacing old 'home-manager-path'
-installing 'home-manager-path'
-Activating linkGeneration
-Cleaning up orphan links from /home/alex
-Creating profile generation 20
-Creating home file links in /home/alex
-Activating onFilesChange
-Activating reloadSystemd
-
-There are 160 unread and relevant news items.
-Read them by running the command "home-manager news".
-
-
-[alex@glove:~]$ cat .config/home-manager/bash/default.
-nix
-{ pkgs, config, host, username, lib, ... }: let
-  inherit (lib) mkOption types;
-in {
-  options.hmModules.core.bash = mkOption {
-    enabled = mkOption {
-      type = types.bool;
-      default = true;
-    };
-  };
-
-  config = {
-    programs.bash = {
-      enable = true;
-      enableCompletion = true;
-      profileExtra = ''
-        # guh
-      '';
-      shellAliases = {
-        ff = "fastfetch";     
-      };
-      bashrcExtra = '' 
-        eval "$(starship init bash)"
-      '';
-    };
-  };
-}
-
-
-[alex@glove:~]$ cat .config/home-manager/home.nix
 { config, pkgs, ... }:
 
 {
@@ -105,6 +12,7 @@ in {
     ./rofi/default.nix
     ./qutebrowser/default.nix
     ./bash/default.nix
+    ./dunst/default.nix
   ];
 
   # Set the Home Manager state version
@@ -120,6 +28,7 @@ in {
     pkgs.foot
     pkgs.rofi
     pkgs.qutebrowser
+    pkgs.dunst
   ];
 
   # Define files to be managed by Home Manager
