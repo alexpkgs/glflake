4e45cc04113cfde57d1c727ced105ace13fdda01
flover.nix <alexzsk@outlook.com>
2024-08-30 03:13:12 -0400

    Update default.nix

--- home-manager/foot/default.nix
+++ home-manager/foot/default.nix
@@ -5,15 +5,13 @@
       main = {
         app-id = "foot";
         title = "foot";
-        font = "uzura_font:size=12,mplus12:size=10";
-        font-bold = "uzura_font:size=12,mplus12:size=10";
-        font-italic = "uzura_font:size=12,mplus12:size=10";
-        font-bold-italic = "uzura_font:size=12,mplus12:size=10";
-        line-height = 12.2;
-        letter-spacing = 1.12;
+        font = "uzura_font:size=12,mplus12:size=12";
+        font-bold = "uzura_font:size=12:size=12";
+        font-italic = "uzura_font:size=12,size=12";
+        font-bold-italic = "uzura_font:size=12";
         vertical-letter-offset = 1.0;
         dpi-aware = false;
-        pad = "20x15 center"; # optionally append 'center'
+        pad = "30x30 center";
         initial-window-size-pixels = "640x640";
       };
       mouse = {
@@ -21,28 +19,28 @@
         alternate-scroll-mode = "yes";
       };
       cursor = {
-        color = "191724 e0def4";
+        color = "000000 ffffff";
       };
       colors = {
         alpha = 0.94;
-        background = "191c25";
-        foreground = "c5c4d4";
-        regular0 = "525566";
-        regular1 = "a994b8";
-        regular2 = "77adb1";
-        regular3 = "c5c4d4";
-        regular4 = "889bb4";
-        regular5 = "a994b8";
-        regular6 = "77adb1";
-        regular7 = "c5c4d4";
-        bright0 = "666b7f";
-        bright1 = "cdb6dd";
-        bright2 = "94cdd1";
-        bright3 = "f0ecfe";
-        bright4 = "afc5de";
-        bright5 = "cdb6dd";
-        bright6 = "94cdd1";
-        bright7 = "f0ecfe";
+        background = "000000";
+        foreground = "ffffff";
+        regular0 = "000000";
+        regular1 = "333333";
+        regular2 = "666666";
+        regular3 = "999999";
+        regular4 = "cccccc";
+        regular5 = "999999";
+        regular6 = "666666";
+        regular7 = "333333";
+        bright0 = "000000";
+        bright1 = "333333";
+        bright2 = "666666";
+        bright3 = "999999";
+        bright4 = "cccccc";
+        bright5 = "999999";
+        bright6 = "666666";
+        bright7 = "ffffff";
       };
     };
   };
