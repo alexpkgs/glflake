78a6e0505f35a85b8006c512261291b553d3be76
alex.nix <alexzsk@outlook.com>
2024-08-29 21:58:09 -0400

    Update default.nix

---
+++ home-manager/foot/default.nix
@@ -0,0 +1,49 @@
+{
+  programs.foot = {
+    enable = true;
+    settings = {
+      main = {
+        app-id = "foot";
+        title = "foot";
+        font = "uzura_font:size=12,mplus12:size=10";
+        font-bold = "uzura_font:size=12,mplus12:size=10";
+        font-italic = "uzura_font:size=12,mplus12:size=10";
+        font-bold-italic = "uzura_font:size=12,mplus12:size=10";
+        line-height = 12.2;
+        letter-spacing = 1.12;
+        vertical-letter-offset = 1.0;
+        dpi-aware = false;
+        pad = "20x15 center"; # optionally append 'center'
+        initial-window-size-pixels = "640x640";
+      };
+      mouse = {
+        hide-when-typing = "yes";
+        alternate-scroll-mode = "yes";
+      };
+      cursor = {
+        color = "191724 e0def4";
+      };
+      colors = {
+        alpha = 0.94;
+        background = "191c25";
+        foreground = "c5c4d4";
+        regular0 = "525566";
+        regular1 = "a994b8";
+        regular2 = "77adb1";
+        regular3 = "c5c4d4";
+        regular4 = "889bb4";
+        regular5 = "a994b8";
+        regular6 = "77adb1";
+        regular7 = "c5c4d4";
+        bright0 = "666b7f";
+        bright1 = "cdb6dd";
+        bright2 = "94cdd1";
+        bright3 = "f0ecfe";
+        bright4 = "afc5de";
+        bright5 = "cdb6dd";
+        bright6 = "94cdd1";
+        bright7 = "f0ecfe";
+      };
+    };
+  };
+}
