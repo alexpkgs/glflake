385b4fcbffd035a7001aab772a3af86788e9abdd
flover.nix <alexzsk@outlook.com>
2024-08-30 18:07:44 -0400

    Create default.nix

---
+++ home-manager/fastfetch/default.nix
@@ -0,0 +1,41 @@
+{ pkgs, configs, ... }:
+{
+  programs.fastfetch = {
+    enable = true;
+    package = pkgs.fastfetch;
+    settings = {
+      logo = {
+        type = "auto";
+        source = "/home/alex/.config/home-manager/fastfetch/tux";
+        width = 44;
+        height = 15;
+      };
+      display = {
+        separator = "";
+      };
+      modules = [
+        {
+          type = "title";
+          format = "{1}@{2}";
+        }
+        { type = "break"; }
+        {
+          type = "wm";
+          key = " ";
+        }
+        {
+          type = "terminal";
+          key = " ";
+        }
+        {
+          type = "theme";
+        }
+        {
+          type = "colors";
+          symbol = "square";
+          paddingLeft = 0;
+        }
+      ];
+    };
+  };
+}
