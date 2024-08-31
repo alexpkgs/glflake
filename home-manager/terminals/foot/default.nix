17e7aaf062f03e74c8f457c9aa3020be350f5403
glove <alexzsk@outlook.com>
2024-08-31 19:06:19 -0400

    Create default.nix

---
+++ home-manager/terminals/foot/default.nix
@@ -0,0 +1,47 @@
+{
+  programs.foot = {
+    enable = true;
+    settings = {
+      main = {
+        app-id = "foot";
+        title = "foot";
+        font = "Fira Code Light:size=10,mplus10:size=10";
+        font-bold = "Fira Code Light:size=10:size=10";
+        font-italic = "Fira Code Light:size=10,size=10";
+        font-bold-italic = "Fira Code Light:size=11";
+        vertical-letter-offset = 1.0;
+        dpi-aware = false;
+        pad = "30x30 center";
+        initial-window-size-pixels = "640x640";
+      };
+      mouse = {
+        hide-when-typing = "yes";
+        alternate-scroll-mode = "yes";
+      };
+      cursor = {
+        color = "000000 ffffff";
+      };
+      colors = {
+        alpha = 0.93;
+        background = "000000";
+        foreground = "ffffff";
+        regular0 = "000000";
+        regular1 = "333333";
+        regular2 = "666666";
+        regular3 = "999999";
+        regular4 = "cccccc";
+        regular5 = "999999";
+        regular6 = "666666";
+        regular7 = "333333";
+        bright0 = "000000";
+        bright1 = "333333";
+        bright2 = "666666";
+        bright3 = "999999";
+        bright4 = "cccccc";
+        bright5 = "999999";
+        bright6 = "666666";
+        bright7 = "ffffff";
+      };
+    };
+  };
+}
