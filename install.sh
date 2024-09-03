1bf18826e63e2aba2a03c5024a1f238c68f164a6
glove <alexzsk@outlook.com>
2024-09-02 23:30:46 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+if [ -n "$(grep -i nixos < /etc/os-release)" ]; then
+  echo "this is nixos"
+  echo "-----"
+else
+  echo "i swear of god go use nixos if you wanna use these dotfiles"
+  exit
+fi
+
+if command -v git &> /dev/null; then
+  echo "git is installed thank you"
+  echo "-----"
+else
+  echo "git is not installed please install git (git gud)"
+  echo "Example: nix-shell -p git"
+  exit
+fi
