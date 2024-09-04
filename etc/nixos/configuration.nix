85596c7828dd9145eabbd9b0d87ce716e1b8c705
glove.tux <alexzsk@outlook.com>
2024-09-04 19:19:47 -0400

    removing useless stuff i might not need in configuration.nix

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -15,10 +15,6 @@
   networking.hostName = "lenovo"; # Define your hostname.
   networking.networkmanager.enable = true;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Services
   services.xserver.enable = true;
   services.xserver.layout = "us";
