555cb08ad010c8ce9798db98e8c87fde7d1399b4
alexpkgs <alexzsk@outlook.com>
2024-09-11 16:40:40 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,14 +33,15 @@ mv walls ~/
 swww img ~/walls/wallpaper.jpg
 
 # upd nixos
-cd /etc/nixos || exit
-sudo rm -rf configuration.nix
-sudo mv configuration.nix /etc/nixos/
-sudo mv intel-drivers.nix /etc/nixos/
+sudo rm -rf /etc/nixos/configuration.nix
+sudo mv ~/glflake/etc/nixos/configuration.nix /etc/nixos/
+sudo mv ~/glflake/etc/nixos/intel-drivers.nix /etc/nixos/
+cd /home/alex
 
 # gdm fix just in case if it gets smh wrong
 nix profile install flake:nixpkgs#legacyPackages.x86_64-linux.gdm --priority 6
-
+nix profile install nixpkgs#gdm
+nix-env -iA nixos.gdm # do not the nix-env sorry i had to do this same for the font
 # nix stuff and emacs
 nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz home-manager
 nix-channel --update
@@ -48,6 +49,7 @@ git clone https://github.com/alexzsk/glomacs.git
 cd glomacs
 mv emacs ~/.config
 nix profile install nixpkgs#fira-code
+nix-env -iA nixos.fira-code
 
 # hm
 if nix-shell '<home-manager>' -A install; then
@@ -61,7 +63,7 @@ fi
 if home-manager switch; then
   echo "home-manager appiled"
 else
-  echo "qhar"
+  echo "please restart"
   exit 1
 fi
 
@@ -69,6 +71,6 @@ fi
 if sudo nixos-rebuild switch; then
   echo "updated"
 else
-  echo "qhar"
+  echo "okay so thats pretty werid try restarting"
   exit 1
 fi
